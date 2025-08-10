7375f02fa416b1c44fe4b2eda3b26659bc4fe833
Avisek Sahoo <avisek@subitup.com>
2025-08-10 19:33:03 +0530

    token issue solved

--- src/navigations/Router.tsx
+++ src/navigations/Router.tsx
@@ -1,24 +1,27 @@
-// External Packages import
-import React, { useEffect, useState } from 'react';
-// Internal file import
+import React, { useEffect } from 'react';
 import AuthNavigations from './AuthStack';
 import MainNavigation from './MainStack';
-import { useAppContext } from '../Context';
 import { useAppDispatch, useAppSelector } from '../hooks';
 import { getItem } from '../utils';
-import { logIn } from '../features';
+import { logIn, setCredentials } from '../features'; // make sure setCredentials updates token in Redux
 
 const Routes = () => {
   const dispatch = useAppDispatch();
   const { isLoggedIn } = useAppSelector(state => state?.auth);
-  const authCheck = getItem('Login');
-  console.log('authCheck', authCheck);
-  React.useEffect(() => {
-    if (authCheck) {
-      dispatch(logIn());
-    } else {
-    }
-  }, [authCheck]);
+
+  useEffect(() => {
+    const restoreAuth = async () => {
+      const loginFlag = await getItem('isLoggedIn');
+      const token = await getItem('accessToken'); // already parsed
+
+      if (loginFlag === 'true' && token) {
+        dispatch(setCredentials({ token })); // no JSON.parse needed
+        dispatch(logIn());
+      }
+    };
+
+    restoreAuth();
+  }, []);
 
   return <>{isLoggedIn ? <MainNavigation /> : <AuthNavigations />}</>;
 };
